293bdd2c6cd88830a886c4c5b09bb96b4f09d29e
Lindsay Trenton <lt353@hawaii.edu>
2025-04-16 16:25:04 -1000

    Initial set up from the Lab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react';
+import Game from "./Game";
 
-function App() {
+function App() {  
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>This is a simple template react app.</p>
-      <p>Feel free to modify it as you like.</p>
-      <p>Happy coding!</p>
-    </div>
+    <Game />
   );
 }
 
